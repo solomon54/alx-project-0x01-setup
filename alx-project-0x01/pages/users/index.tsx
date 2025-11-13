3ced3a13afab6ae4d05ea4b5ec53c2b342d7e9ce
solomon54 <solomontsehay54@gmail.com>
2025-11-13 22:00:33 +0300

    Implement Users Card Components

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,26 +1,73 @@
 import Header from "@/components/layout/Header";
+import { UserProps } from "@/interfaces";
+import UserCard from "@/components/common/UserCard";
+import { useState } from "react";
+import { compile } from "tailwindcss";
 
-export default function UsersPage({ posts }: { posts: any[] }) {
+const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
+  console.log(posts);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Header />
-      <main className="p-6">
-        <h1 className="text-3xl font-bold">Users Page</h1>
-        <ul className="mt-4 space-y-2">
-          {posts.map((user) => (
-            <li key={user.id} className="p-4 border rounded-md">
-              <strong>{user.name}</strong> — @{user.username}
-            </li>
-          ))}
-        </ul>
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className=" text-2xl font-semibold">User Content</h1>
+          <button
+            onClick={handleOpenModal}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+          >
+            Add User
+          </button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 ">
+          {posts?.map(
+            (
+              {
+                name,
+                email,
+                phone,
+                website,
+                id,
+                username,
+                address,
+                company,
+              }: UserProps,
+              key: number
+            ) => (
+              <UserCard
+                name={name}
+                email={email}
+                phone={phone}
+                id={id}
+                company={company}
+                username={username}
+                address={address}
+                website={website}
+                key={key}
+              />
+            )
+          )}
+        </div>
       </main>
-    </>
+      
+    </div>
   );
-}
+};
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const posts = await response.json();
 
-  return { props: { posts } };
+  return {
+    props: {
+      posts,
+    },
+  };
 }
+export default Users;
